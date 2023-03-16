5da2188c0d709b464405f01b8c8526b7f5f53174
brandontyruspetty <tyrus1974@gmail.com>
2023-03-16 10:19:02 -0700

    update chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, color } = route.params;
+  const [messages, setMessages] = useState([]);
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => 
+      GiftedChat.append(previousMessages, newMessages))
+  }
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: name, color: color });
   }, []);
 
     return (
-      <View style={[styles.container, {backgroundColor: color}]}>
-        <Text>Welcome to the Chat</Text>
+      <View style={[styles.container, {backgroundColor: color}]}
+      >
+       <GiftedChat
+          messages={messages}
+          onSend={messages => onSend(messages)}
+          user={{
+            _id: 1
+          }}
+        />
+        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
       </View>
-    );
-  }
-
+   )
+ };
+      
 const styles = StyleSheet.create({
  container: {
    flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center'
  }
 });
 
